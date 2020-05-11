a912c4d36d52075cf26e5c20e47ead2a107a5e28
Cristobal Salazar <chris.salza@gmail.com>
2020-05-10 23:02:07 -0400

    renamed routes to controllers

--- server/api/controllers/auth.js
+++ server/api/controllers/auth.js
@@ -1,10 +1,7 @@
 require('dotenv').config()
 const router = require('express').Router()
-const bcrypt = require('bcrypt')
 const db = require('../models')
-const jwt = require('jsonwebtoken')
-
-const tokenDuration = '1d'
+const encryption = require('../services/encryption')
 
 function unauthorized(res) {
   res.status(401).json({ message: 'Unauthorized' })
@@ -12,14 +9,10 @@ function unauthorized(res) {
 
 router.post('/refresh', async (req, res) => {
   try {
-    const decoded = jwt.verify(req.body.token, process.env.API_KEY)
-    const { email, uid, username } = decoded
-    const token = jwt.sign({ email, uid, username }, process.env.API_KEY, {
-      expiresIn: tokenDuration
-    })
+    const { email, uid, username } = encryption.decode(req.body.token)
+    const token = encryption.sign({ email, uid, username })
     res.json({ token, user: { email, uid, username } })
   } catch (err) {
-    console.log(JSON.stringify(err, null, 2))
     unauthorized(res)
   }
 })
@@ -28,40 +21,46 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body
   const user = await db.User.findOne({ email })
   if (!user) {
-    console.log('No user found')
+    console.log('user does not exist')
     return unauthorized(res)
   }
-
-  bcrypt.compare(password, user.password, (err, same) => {
-    if (err || !same) return unauthorized(res)
-    const payload = {
-      uid: user._id,
-      username: user.username,
-      picture: user.picture,
-      friends: user.friends
+  try {
+    const validPassword = await encryption.compareHash(
+      password,
+      user.password
+    )
+    if (!validPassword) {
+      return unauthorized(res)
+    } else {
+      const token = encryption.sign({
+        uid: user._id,
+        username: user.username,
+        picture: user.picture,
+        friends: user.friends,
+      })
+      res.json({ token, user: payload })
     }
-    const token = jwt.sign(payload, process.env.API_KEY, {
-      expiresIn: tokenDuration
-    })
-    res.json({ token, user: payload })
-  })
+  } catch (err) {
+    console.log(err)
+    return unauthorized(res)
+  }
 })
 
 router.post('/register', async (req, res) => {
   const { email, password, username } = req.body
   const userExists = await db.User.findOne({ email })
-  if (userExists) return res.status(304).end()
+  if (userExists) return res.status(400).end()
   const user = await db.User.create({
     username,
     email,
     password,
-    friends: []
+    friends: [],
   })
   res.json({
     _id: user._id,
     username: user.username,
     email: user.email,
-    picture: user.picture
+    picture: user.picture,
   })
 })
 
